ecec64cad3de683f2bbdbb55545c0338a67703e2
zbb1211 <1406345211@qq.com>
2018-10-17 14:50:34 +0800

    '2-2请求api获取天气数据'

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,16 @@
 //index.js
-Page({})
+Page({
+  onLoad() {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '深圳市'
+      },
+      success(res) {
+        const { data: { result } } = res;
+        const { now: { temp, weather } } = result;
+        console.log(temp, weather);
+      }
+    })
+  }
+})
