f7357ddda7613ca24d3e13bb6238b1848599c4f7
zbb1211 <1406345211@qq.com>
2018-10-17 15:46:21 +0800

    '动态改变图片和导航栏背景色'

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,16 +1,45 @@
 //index.js
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+const colorMap = {
+  'sunny': '#c4efff',
+  'cloudy': '#daeff7',
+  'overcast': '#c4ced2',
+  'lightrain': '#b6d6e2',
+  'heavyrain': '#c3ccd0',
+  'snow': '#99e3ff'
+}
 Page({
+  data: {
+    temp: 0,
+    weather: '',
+    imgSrc: ''
+  },
   onLoad() {
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now',
       data: {
         city: '深圳市'
       },
-      success(res) {
+      success: (res) => {
         const { data: { result } } = res;
         const { now: { temp, weather } } = result;
-        console.log(temp, weather);
+        this.setData({
+          temp: temp,
+          weather: weatherMap[weather],
+          imgSrc: `/images/${weather}-bg.png`
+        });
+        wx.setNavigationBarColor({
+          backgroundColor: colorMap[weather],
+          frontColor: '#000000'
+      })
       }
-    })
+    });
   }
 })
